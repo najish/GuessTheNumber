aa98bff7fac2467ded7cc2c6aba49178c2d21932
zafer eqbal <najish.eqbal@gmail.com>
2022-07-31 03:18:28 +0530

    typing app in typing.html

---
+++ string.js
@@ -0,0 +1,8 @@
+let a = 5;
+
+do 
+{
+    console.log('najish');
+    a += 1;
+
+}while(a < 10);
---
+++ variable.js
@@ -0,0 +1,8 @@
+let store = '12';
+
+
+console.log(typeof store);
+
+store = Number(store);
+
+console.log(typeof store);

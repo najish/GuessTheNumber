d5e158afc11a4018b7b4ba0f5db00d42a6646af9
zafer eqbal <najish.eqbal@gmail.com>
2022-07-31 18:46:38 +0530

    new file

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const prompt = require("prompt-sync")({ sigint: true });
-const age = prompt("How old are you? ");
-console.log(`You are ${age} years old.`);
+let wait = document.querySelector('.wait');
+let msg = document.querySelector('.alert');
+
+console.log(wait);
+
+
+
+wait.addEventListener('click',(await fun()));
+
+
+msg.addEventListener('click',() => {
+    alert('Hello this is an alert message');
+});
+
+
+async function fun() {
+    // let i = 1;
+    // for(; i <= 10000 * 100000; i++)
+    //     for(let j = 1; j <= 10000 * 100000; j++)
+    //         continue;
+
+    console.log('hey');
+}
---
+++ list.js
@@ -0,0 +1,8 @@
+let a = 30,b = 20;
+function fun() {
+    if(a < b) return false;
+    else 
+        console.log('hello');
+}
+
+console.log(fun());
